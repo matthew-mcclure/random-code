5e64f0960a7ec485793754779c5a2dd03db5f484
Matthew McClure <mcclure0728@gmail.com>
2017-03-22 16:43:29 -0700

    Update no-regex-deep-array-flatten.js

--- flatten-array/no-regex-deep-array-flatten.js
+++ flatten-array/no-regex-deep-array-flatten.js
@@ -4,7 +4,7 @@ function flatten (arr) {
 	var arrAsString = arr.toString()
 	var flatArr = arrAsString.split(',')
 	for(var i = 0; i < flatArr.length; i++) {
-  		flatArr[i] = +flatArr[i]
-  	}
+ 		if(+flatArr[i]) flatArr[i] = +flatArr[i]
+	}
   	return flatArr
 }
